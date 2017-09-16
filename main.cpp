199532dee32b580d3ac590d45ccf946b280367da
ragnarlodbrok1992 <maraus92@gmail.com>
2017-09-16 18:50:52 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/*
+ * GLUT Shapes Demo
+ *
+ * Written by Nigel Stewart November 2003
+ *
+ * This program is test harness for the sphere, cone
+ * and torus shapes in GLUT.
+ *
+ * Spinning wireframe and smooth shaded shapes are
+ * displayed until the ESC or q key is pressed.  The
+ * number of geometry stacks and slices can be adjusted
+ * using the + and - keys.
+ */
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+
+/* GLUT callback Handlers */
+/* Program entry point */
+
+int main(int argc, char *argv[])
+{
+
+    return EXIT_SUCCESS;
+}
